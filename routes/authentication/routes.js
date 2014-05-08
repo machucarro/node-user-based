1ebbef2c563c37ea4a6f29ce547b893b39a4d751
machucarro <manuel.achucarro@gmail.com>
2014-05-08 16:33:14 +0200

    connect-local routes added

--- routes/authentication/routes.js
+++ routes/authentication/routes.js
@@ -40,4 +40,57 @@ module.exports = function (app, authenticationService) {
     app.get('/auth/google', controller.auth.google.authenticate);
     app.get('/auth/google/callback', controller.auth.google.callback({ successRedirect : '/profile', failureRedirect: '/' }));
 
+    // =============================================================================
+    // UNLINK ACCOUNTS =============================================================
+    // =============================================================================
+    // used to unlink accounts. for social accounts, just remove the token
+    // for local account, remove email and password
+    // user account will stay active in case they want to reconnect in the future
+
+    // local -----------------------------------
+    app.get('/unlink/local', function(req, res) {
+        var user            = req.user;
+        user.local.email    = undefined;
+        user.local.password = undefined;
+        user.save(function(err) {
+            res.redirect('/user/profile');
+        });
+    });
+
+    // facebook -------------------------------
+    app.get('/unlink/facebook', function(req, res) {
+        var user            = req.user;
+        user.facebook.token = undefined;
+        user.save(function(err) {
+            res.redirect('/user/profile');
+        });
+    });
+
+    // twitter --------------------------------
+    app.get('/unlink/twitter', function(req, res) {
+        var user           = req.user;
+        user.twitter.token = undefined;
+        user.save(function(err) {
+            res.redirect('/user/profile');
+        });
+    });
+
+    // google ---------------------------------
+    app.get('/unlink/google', function(req, res) {
+        var user          = req.user;
+        user.google.token = undefined;
+        user.save(function(err) {
+            res.redirect('/user/profile');
+        });
+    });
+
+    // github ---------------------------------
+    app.get('/unlink/github', function(req, res) {
+        var user          = req.user;
+        user.github.token = undefined;
+        user.save(function(err) {
+            res.redirect('/user/profile');
+        });
+    });
+
 }
