c4724770b90a931668acfd867003c5d5b36ab714
machucarro <manuel.achucarro@gmail.com>
2014-05-08 15:46:44 +0200

    Public domain routes refactored

--- routes/public_domain/PublicDomainController.js
+++ routes/public_domain/PublicDomainController.js
@@ -1,13 +1,17 @@
-module.exports = {
+module.exports = function PublicDomainController(){
+
+    if(!(this instanceof PublicDomainController)){
+        return new PublicDomainController();
+    }
 
 /*=== GET Pages ==================================================*/
-    get : {
+    this.get = {
         'home': function (req, res) {
             res.render("public_domain/index");
         },
         'ping': function (req, res) {
             res.end("pong!");
         }
-    }
+    };
 };
 
--- routes/public_domain/routes.js
+++ routes/public_domain/routes.js
@@ -1,5 +1,6 @@
-var controller = require("./publicDomainController");
+var PublicDomainController = require("./publicDomainController");
 module.exports = function init(app){
+    var controller = new PublicDomainController();
     app.get('/', controller.get.home);
     app.get('/ping', controller.get.ping);
-}
+};
