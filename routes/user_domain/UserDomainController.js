ed856b22ba6811e90222d733840acec9bd4fb33f
machucarro <manuel.achucarro@gmail.com>
2014-05-08 15:51:33 +0200

    User domain routes refactored

--- routes/user_domain/UserDomainController.js
+++ routes/user_domain/UserDomainController.js
@@ -1,6 +1,11 @@
-module.exports = {
-    profile : {
-        'get': function (req, res) {
+module.exports = function UserDomainController(){
+
+    if(!(this instanceof UserDomainController)){
+        return new UserDomainController();
+    }
+
+    this.get = {
+        'profile': function (req, res) {
             res.render('user_domain/profile', {
                 user : req.user // get the user out of session and pass to template
             });
--- routes/user_domain/routes.js
+++ routes/user_domain/routes.js
@@ -1,5 +1,6 @@
-var controller = require("./userDomainController");
+var UserDomainController = require("./userDomainController");
 
-module.exports = function (app, passport){
-    app.get('/profile', passport.isLoggedIn, controller.profile.get);
-}
+module.exports = function (app, authenticationService){
+    var controller = new UserDomainController();
+    app.get('/profile', authenticationService.isLoggedIn, controller.get.profile);
+};
